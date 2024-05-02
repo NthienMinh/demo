d7db92cbc95f790d2c0dc015d95cf47b63a53696
Minh Nguyễn <88758271+NthienMinh@users.noreply.github.com>
2024-05-02 14:41:14 +0700

    update

--- lib/counter_cubit.dart
+++ lib/counter_cubit.dart
@@ -3,7 +3,11 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 class CounterCubit extends Cubit<int> {
   CounterCubit() : super(0);
 
-  increment() {
+  int number1 = 0;
+  int number2 = 0;
+
+  increment(bool change) {
+    number1++;
     emit(state + 1);
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -39,39 +39,61 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return BlocProvider(
         create: (context) => CounterCubit(),
-        child: BlocBuilder<CounterCubit, int>(builder: (c, state) {
-          return Scaffold(
-            appBar: AppBar(
-              backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-              title: Text(widget.title),
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+            title: Text(widget.title),
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text(
+                  'Bấm vào dấu cộng để bộ đềm thêm 1!',
+                ),
+                const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 10),
+                    child: Text(
+                      'Số đếm hiện tại: ',
+                    )),
+                BlocBuilder<CounterCubit, int>(
+                    builder: (c, state) {
+
+                  var cubit = BlocProvider.of<CounterCubit>(c);
+                  return InkWell(
+                    onTap: (){
+                      cubit.increment(true);
+                    },
+                    child: Text(
+                      '${cubit.number1} == $state',
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                  );
+                }),
+                BlocBuilder<CounterCubit, int>(builder: (cc, s) {
+                  var cubit = BlocProvider.of<CounterCubit>(cc);
+                  return InkWell(
+                    onTap: (){
+                      cubit.increment(false);
+                    },
+                    child: Text(
+                      '${cubit.number2} == $s',
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                  );
+                }),
+
+
+              ],
             ),
-            body: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  const Text(
-                    'Bấm vào dấu cộng để bộ đềm thêm 1!',
-                  ),
-                  const Padding(
-                      padding: EdgeInsets.symmetric(vertical: 10),
-                      child: Text(
-                        'Số đếm hiện tại: ',
-                      )),
-                  Text(
-                    '$state',
-                    style: Theme.of(context).textTheme.headlineMedium,
-                  ),
-                ],
-              ),
-            ),
-            floatingActionButton: FloatingActionButton(
-              onPressed: (){
-                BlocProvider.of<CounterCubit>(c).increment();
-              },
-              tooltip: 'Increment',
-              child: const Icon(Icons.add),
-            ), // This trailing comma makes auto-formatting nicer for build methods.
-          );
-        }));
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: (){
+
+            },
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ), // This trailing comma makes auto-formatting nicer for build methods.
+        ) );
   }
 }
